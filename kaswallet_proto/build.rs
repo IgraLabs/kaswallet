7d6d31d62e8cc1839c633ecc5db3d75256b0f6e0
Mike Zak <feanorr@gmail.com>
2025-03-31 11:20:21 +0300

    Move kaswallet-proto to it's separate repository

--- kaswallet_proto/build.rs
+++
@@ -1,6 +0,0 @@
-﻿fn main() {
-    tonic_build::configure()
-        .out_dir("src")
-        .compile_protos(&["proto/wallet.proto"], &["proto"])
-        .unwrap_or_else(|e| panic!("Failed to compile proto file: {:?}", e));
-}
--- kaswallet_proto/src/lib.rs
+++
@@ -1,1 +0,0 @@
-pub mod kaswallet_proto;
